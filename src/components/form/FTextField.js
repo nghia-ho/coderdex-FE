0d1741c4c700b191478bbbb0683cd43227d2066f
Nghia-ho <nguyenhoannghia.work@gmail.com>
2022-10-12 13:31:59 +0700

    first commit

--- src/components/form/FTextField.js
+++ src/components/form/FTextField.js
@@ -2,23 +2,24 @@ import { useFormContext, Controller } from "react-hook-form";
 import { TextField } from "@mui/material";
 
 function FTextField({ name, ...other }) {
-    const { control } = useFormContext();
+  const { control } = useFormContext();
 
-    return (
-        <Controller
-            name={name}
-            control={control}
-            render={({ field, fieldState: { error } }) => (
-                <TextField
-                    {...field}
-                    fullWidth
-                    error={!!error}
-                    helperText={error?.message}
-                    {...other}
-                />)
-            }
+  return (
+    <Controller
+      defaultValue={""}
+      name={name}
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          fullWidth
+          error={!!error}
+          helperText={error?.message}
+          {...other}
         />
-    );
+      )}
+    />
+  );
 }
 
 export default FTextField;
